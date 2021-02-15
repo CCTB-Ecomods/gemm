4d547e17234a6fb1169a4bb694be96517ecff6b3
Daniel Vedder <d.vedder@web.de>
2021-02-15 16:59:57 +0100

    Added configuration files for the trait-constrained scenario

--- runsim.sh
+++ runsim.sh
@@ -4,11 +4,14 @@
 
 # Usage: `./runsim.sh <scenario> <setting> <replicate>`, where:
 #        scenario: "constant", "variable"
-#        setting: "small", "reflecting", "absorbing"
+#        setting: "small", "reflecting", "absorbing", "constrained"
 #        replicate: an integer determining the random seed
 
+
+# produce a new config file for this run, updating its output directory and seed
 sed -e "s/$1_$2/$1_$2_$3/" \
 	-e "s/seed 0/seed $3/" \
 	examples/gradient/$1_$2.conf > $1_$2_$3.conf
 
+# run the simulation
 ./rungemm.jl -c $1_$2_$3.conf
