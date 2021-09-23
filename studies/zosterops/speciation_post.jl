de8b66c084de98df0bf596fccc084982723fca88
rroelz <robin.roelz@stud-mail.uni-wuerzburg.de>
2021-09-23 17:31:02 +0200

    missed debug change

--- studies/zosterops/speciation_post.jl
+++ studies/zosterops/speciation_post.jl
@@ -88,8 +88,7 @@ grab all results folders and returns one according to the SLURM_ARRAY_TASK_ID
 
 function get_results(results_path)
     folders = readdir(results_path)
-    #task_id = Base.parse(Int, ENV["SLURM_ARRAY_TASK_ID"])
-    task_id = 3
+    task_id = Base.parse(Int, ENV["SLURM_ARRAY_TASK_ID"])
     taskfolder = folders[task_id]
     return joinpath(results_path, taskfolder)
 end
