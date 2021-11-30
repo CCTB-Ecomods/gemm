acfc160acc9529b1e9499060d03e08045f0c665d
rroelz <robin.roelz@stud-mail.uni-wuerzburg.de>
2021-11-30 13:19:34 +0100

    changed islsim.sh for tests

--- studies/islandradiation/islsim.sh
+++ studies/islandradiation/islsim.sh
@@ -2,7 +2,7 @@
 
 echo "Copying input files."
 
-cp studies/islandradiation/islsim.config .
+cp studies/islandradiation/islsim_test.config .
 #TODO Does this map work for short tests
 cp studies/islandradiation/islsim_test.map .
 #cp studies/islandradiation/islsim.map .
@@ -11,12 +11,12 @@ rm -r results/islsim_test #remove after testing
 echo "Starting simulation run."
 
 date
-time julia -t auto rungemm.jl --config islsim_test.config
+julia rungemm.jl --config islsim_test.config
 date
 
 echo "Deleting input files."
 
-rm islsim.config
+rm islsim_test.config
 #TODO change back to real simulation map
 rm islsim_test.map
 #rm islsim.map
