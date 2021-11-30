d4ed308996ca7289c84acadb3cb913911ca994eb
rroelz <robin.roelz@stud-mail.uni-wuerzburg.de>
2021-11-30 17:57:05 +0100

    set up debugging

---
+++ rungemm_debug.jl
@@ -0,0 +1,15 @@
+#!/usr/bin/env julia
+#Robin - copy of rungemm.jl for debugging in VScode
+
+thisDir = joinpath(pwd(), "src")
+any(path -> path == thisDir, LOAD_PATH) || push!(LOAD_PATH, thisDir)
+using Pkg
+Pkg.activate(".")
+using GeMM
+
+# remove od folder with results
+remove_old_results = `rm -r results/islsim_test`
+run(remove_old_results)
+
+#run gemm directly from .config file
+rungemm("studies/islandradiation/islsim_test.config", 0)
