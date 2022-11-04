c4dbc1e82cdb6ce7b2aed43ce707abd7ddecf63c
rroelz <robin.roelz@stud-mail.uni-wuerzburg.de>
2022-11-04 19:12:37 +0100

    wrote some utility functions to test

--- src/tests.jl
+++ src/tests.jl
@@ -37,25 +37,69 @@ using
     include("scheduling.jl")
     
     include("run_simulation.jl")
-    
-#TODO: OOF the settings think is needed sadly :upsidedownface:
-function testmap(mapfilename::String) 
+ 
+
+"""
+    testworld(mapfilename)
+
+Creating a world for testing from a file
+"""
+function testworld(mapfilename::String, full::Bool = false) 
     maptable = basicparser(mapfilename)[2:end]
     world = Array{Patch}(undef, length(maptable))
     for entry in eachindex(maptable)
         newpatch = createpatch(maptable[entry])
+        if full && newpatch.initpop
+           append!(newpatch.community, testfill(newpatch)) 
+        end
         world[entry] = newpatch
     end
+    global newpatch = nothing # remove variable used in `createpatch()`
+    findneighbours!(world)
     return world
-
-#create a world completely in here
-#populate it fully
-#return it
 end
+"""
+    testfill(patch::Patch)
 
-```
+hacky version based on 'zgenesis' returns an array to fully fill a patch with pairs of zosterops
+"""
+function testfill(patch::Patch)
+    community = Array{Individual, 1}()
+    npairs = Integer(round(patch.capacity/2))
+    for i in 1:npairs
+        m = testspawn(male)
+        f = testspawn(female)
+        f.partner = m.id
+        m.partner = f.id
+        push!(community, m)
+        push!(community, f)
+    end
+    return community
+end
+"""
+    testspawn(sex)
+
 create an individual in a specific patch
-```
-function testspawn() #take settings from where?
-    # make an individual in a place
+"""
+function testspawn(sex::Sex) #take settings from where?
+    species = setting("species")[1]["lineage"]
+    bird = getzosteropsspecies(species, sex)
+    return bird
 end
+"""
+    testsettings(config, seed)
+
+get the settings for the testing. logging is turned off by default to test in console
+"""
+function testsettings(config::String = "", seed::Integer = 0)
+    initsettings(defaultSettings())
+    initsettings(getsettings(config, seed))
+    updatesetting("logging", false)
+    Random.seed!(setting("seed"))
+end
+
+testsettings("test.config")
+world = testworld("map100.map", true)
+hubert = testspawn(male)
+world[4995].seedbank = [hubert] #this is coordinate 50,50 use `findcoordinate()`
+zdisperse!(world)
