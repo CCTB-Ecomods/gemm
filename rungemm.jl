856186cd1b62dac5ede7f2ffda5f73eb7f3c0ffb
Daniel Vedder <daniel.vedder@idiv.de>
2022-11-24 12:49:48 +0100

    Created Project.toml and Manifest.toml files

--- rungemm.jl
+++ rungemm.jl
@@ -3,8 +3,6 @@
 # have a look at `rungemmparallel.jl`, or import the GeMM module into your own
 # wrapper script.
 
-thisDir = joinpath(pwd(), "src")
-any(path -> path == thisDir, LOAD_PATH) || push!(LOAD_PATH, thisDir)
 using Pkg
 Pkg.activate(".")
 using GeMM
